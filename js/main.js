e71e195ca26b2a513298aa44f983c1bfd62232a5
Sharaiyri Rami <sharaiyri@mediamarktsaturn.com>
2018-04-22 11:50:58 +0300

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function activateDropDown(objId) {
+    document.getElementById(objId).classList.toggle("show");
+}
---
+++ js/request.js
@@ -0,0 +1,30 @@
+function fireAPICall() {
+    var rsp = '{"rami" : 1}';
+    $.ajax({
+        url: `https://reqres.in/api/users/2`,
+        type: 'GET',
+        data: {
+            format: 'json'
+        },
+        success: function(response) {
+            rsp = JSON.stringify(response);
+            $('#responseTextField').attr('placeholder', rsp);
+            //console.log(response)
+        },
+        error: function() {
+            //console.log("error")
+        }
+    });
+
+    $('#dropdown-menu2').show();
+}
+
+function clearFunction() {
+    if ($('#responseTextField').attr('placeholder') == '') {
+        $('#responseTextField').attr('placeholder', 'Label is empty already');
+        return;
+    }
+
+    $('#dropdown-menu2').hide();
+    $('#responseTextField').attr('placeholder', 'Response will be printed here');
+}
