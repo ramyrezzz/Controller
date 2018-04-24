0955059db66d9a227f47c3771e84f27142281d30
Sharaiyri Rami <sharaiyri@mediamarktsaturn.com>
2018-04-24 09:16:36 +0300

    Added Reload Function to refresh button

--- js/main.js
+++ js/main.js
@@ -3,3 +3,6 @@ toggle between hiding and showing the dropdown content */
 function activateDropDown(objId) {
     document.getElementById(objId).classList.toggle("show");
 }
+function reload() {
+    location.reload()
+}
