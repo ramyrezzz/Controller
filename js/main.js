347b8d2d6ad9aff639f736a85963933870718284
Sharaiyri Rami <sharaiyri@mediamarktsaturn.com>
2018-04-24 15:37:52 +0300

    Implemented a functino that would hide the drop-down menu when out of focus

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
+
+$(document).mouseup(function(e) {
+    var container = $(".dropdown-menu");
+    // if the target of the click isn't the container nor a descendant of the container
+    if (!container.is(e.target) && container.has(e.target).length === 0)
+        container.hide();
+});
+
 function activateDropDown(objId) {
     document.getElementById(objId).classList.toggle("show");
 }
@@ -15,10 +23,8 @@ function buildDivForBranch(divname, list) {
     if ($('#select-Branch').children().length > 0)
         return;
 
-    for(i = 0; i < list.length; i++) {
-        // html += "<option value='" + branches[i] + "'>" + branches[i] + "</option>";
+    for(i = 0; i < list.length; i++)
         html += '<a href=\"#\" class=\"dropdown-item\" onclick=\"deployFunction(\'' +  list[i] + "\')\" id=\"" +  list[i] + '\"' + "<p>Fire <strong>" + list[i] + "</strong>" + ' ' +  'API call' + '</p></a>' ;
-    }
 
     html += '</select>';
     newDiv.innerHTML= html;
